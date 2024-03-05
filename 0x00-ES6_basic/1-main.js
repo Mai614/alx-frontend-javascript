7e5b979031a48b3ae2834426ad8292d7850a4fef
Mai614 <maymohamed614@gmail.com>
2024-03-05 20:13:26 +0000

    fix  Block Scope

--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -1,4 +1,5 @@
-import taskBlock from './1-block-scoped.js';
+import taskBlock from '/alx-frontend-javascript/0x00-ES6_basic/1-block-scoped.js';
 
 console.log(taskBlock(true));
 console.log(taskBlock(false));
+
