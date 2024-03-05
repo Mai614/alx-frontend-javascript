76e840266bce9dbd4f47ced55ad2e1f1ef946d45
Mai614 <maymohamed614@gmail.com>
2024-03-05 16:21:01 +0000

    2-main

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import getNeighborhoodsList from '/alx-frontend-javascript/0x00-ES6_basic/2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
