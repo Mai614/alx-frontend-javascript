c72702278aec41e903d409126d53eca1b1ee7771
Mai614 <maymohamed614@gmail.com>
2024-03-05 20:21:24 +0000

    Rest parameter

---
+++ 0x00-ES6_basic/4-main.js
@@ -0,0 +1,4 @@
+import returnHowManyArguments from '/alx-frontend-javascript/0x00-ES6_basic/4-rest-parameter.js';
+
+console.log(returnHowManyArguments("one"));
+console.log(returnHowManyArguments("one", "two", 3, "4th"));
