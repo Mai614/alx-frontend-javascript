389dbe240380d242d20420e24980db7ff6e362db
Mai614 <maymohamed614@gmail.com>
2024-03-07 14:33:28 +0000

    fix22

--- 0x02-ES6_classes/1-main.js
+++ 0x02-ES6_classes/1-main.js
@@ -1,3 +1,3 @@
-import initializeRooms from './1-make_classrooms.js';
+import initializeRooms from '/alx-frontend-javascript/0x02-ES6_classes/1-make_classrooms.js';
 
 console.log(initializeRooms());
