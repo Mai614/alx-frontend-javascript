2be7e243086b95c21372e53e1691ec6ab1a88a33
Mai614 <maymohamed614@gmail.com>
2024-03-07 23:54:44 +0000

    task11

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,5 @@
-import ClassRoom from '/alx-frontend-javascript/0x02-ES6_classes/0-classroom.js';
+import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34)
-  ];
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
-
-export default initializeRooms;
