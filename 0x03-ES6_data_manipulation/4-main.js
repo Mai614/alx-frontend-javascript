3ec242b1c8839fa3995d429a3a9b80c1caa70628
Mai614 <maymohamed614@gmail.com>
2024-03-12 23:04:35 +0000

    Combine

---
+++ 0x03-ES6_data_manipulation/4-main.js
@@ -0,0 +1,9 @@
+import getListStudents from
+  "/alx-frontend-javascript/0x03-ES6_data_manipulation/0-get_list_students.js";
+import updateStudentGradeByCity from
+  "/alx-frontend-javascript/0x03-ES6_data_manipulation/4-update_grade_by_city.js";
+
+console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 },
+  { studentId: 1, grade: 86 }]));
+
+console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
