e184cb8f25e75399b407015ee5a218da348ea9c5
Mai614 <maymohamed614@gmail.com>
2024-03-12 22:22:39 +0000

    fix 0-main

--- 0x03-ES6_data_manipulation/0-main.js
+++ 0x03-ES6_data_manipulation/0-main.js
@@ -1,3 +1,4 @@
-import getListStudents from "/alx-frontend-javascript/0x03-ES6_data_manipulation/0-get_list_students.js";
+import getListStudents from
+               "/alx-frontend-javascript/0x03-ES6_data_manipulation/0-get_list_students.js";
 
 console.log(getListStudents());
