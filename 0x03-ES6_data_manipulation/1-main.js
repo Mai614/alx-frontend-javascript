8622ba18a768cb7e9563d353bce0263fed920b12
Mai614 <maymohamed614@gmail.com>
2024-03-12 22:41:17 +0000

    More mapping

---
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -0,0 +1,7 @@
+import getListStudentIds from
+  "/alx-frontend-javascript/0x03-ES6_data_manipulation/1-get_list_student_ids.js";
+import getListStudents from
+  "/alx-frontend-javascript/0x03-ES6_data_manipulation/0-get_list_students.js";
+
+console.log(getListStudentIds("hello"));
+console.log(getListStudentIds(getListStudents()));
